d81df8e986d93127dc2dcfc5567ceafb36511228
Markus Huckriede <markushuckriede@6e441d89-386e-4f24-a919-f584a208bed8.fritz.box>
2025-07-25 16:46:49 +0200

    add function added

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    with open ('blog_posts.json', 'r') as file:
+    with open('blog_posts.json', 'r') as file:
         blog_posts = json.load(file)
     return render_template('index.html', posts=blog_posts)
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        with open('blog_posts.json', 'r') as file:
+            blog_posts = json.load(file)
+
+        new_post = {
+            'title': title,
+            'content': content
+        }
+        blog_posts.append(new_post)
+
+        with open('blog_posts.json', 'w') as file:
+            json.dump(blog_posts, file, indent=4)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
