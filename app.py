5fd84391b0624b21fc264f0cd44b4c31ed03ce67
Markus Huckriede <markushuckriede@MacBook-Air-von-Markus.fritz.box>
2025-08-05 15:21:42 +0200

    add + update added

--- app.py
+++ app.py
@@ -31,3 +31,46 @@ def add():
         return redirect(url_for('index'))
 
     return render_template('add.html')
+
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    with open('blog_posts.json', 'r') as file:
+        blog_posts = json.load(file)
+
+    updated_posts = [post for post in blog_posts if post['id'] != post_id]
+
+    with open('blog_posts.json', 'w') as file:
+        json.dump(updated_posts, file, indent=4)
+
+    return redirect(url_for('index'))
+
+
+def fetch_post_by_id(post_id):
+    with open('blog_posts.json', 'r') as file:
+        blog_posts = json.load(file)
+    for post in blog_posts:
+        if post.get('id') == post_id:
+            return post
+    return None
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    with open('blog_posts.json', 'r') as file:
+        blog_posts = json.load(file)
+
+    post = next((post for post in blog_posts if post.get('id') == post_id), None)
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        post['title'] = request.form.get('title')
+        post['content'] = request.form.get('content')
+
+        with open('blog_posts.json', 'w') as file:
+            json.dump(blog_posts, file, indent=4)
+
+        return redirect(url_for('index'))
+
+    return render_template('update.html', post=post)
